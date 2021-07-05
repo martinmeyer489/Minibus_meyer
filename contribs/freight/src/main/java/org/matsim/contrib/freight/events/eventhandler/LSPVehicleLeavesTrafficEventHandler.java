c60f2b3b19d77db8b2d428df33ce956343b611da
MeyerMa <martin.meyer@moia.io>
2021-07-05 15:47:17 +0200

    asd

--- contribs/freight/src/main/java/org/matsim/contrib/freight/events/eventhandler/LSPVehicleLeavesTrafficEventHandler.java
+++
@@ -1,9 +0,0 @@
-package org.matsim.contrib.freight.events.eventhandler;
-
-import org.matsim.contrib.freight.events.LSPFreightVehicleLeavesTrafficEvent;
-import org.matsim.core.events.handler.EventHandler;
-
-public interface LSPVehicleLeavesTrafficEventHandler extends EventHandler {
-	
-	public void handleEvent( LSPFreightVehicleLeavesTrafficEvent event );
-}
--- contribs/minibus/src/main/java/org/matsim/contrib/minibus/RunMinibus.java
+++ contribs/minibus/src/main/java/org/matsim/contrib/minibus/RunMinibus.java
@@ -1,3 +1,5 @@
+
+
 /* *********************************************************************** *
  * project: org.matsim.*
  *                                                                         *
@@ -21,48 +23,123 @@ package org.matsim.contrib.minibus;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.contrib.minibus.PConfigGroup;
 import org.matsim.contrib.minibus.hook.PModule;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.population.algorithms.TripsToLegsAlgorithm;
+import org.matsim.core.router.MainModeIdentifier;
+import org.matsim.core.router.MainModeIdentifierImpl;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.pt.PtConstants;
+
+import java.util.Collections;
+
+import static org.matsim.core.config.groups.PlansConfigGroup.HandlingOfPlansWithoutRoutingMode.useMainModeIdentifier;
 
 
 /**
  * Entry point, registers all necessary hooks
- * 
+ *
  * @author aneumann
  */
 public final class RunMinibus {
 
 	private final static Logger log = Logger.getLogger(RunMinibus.class);
-	
-	private final Config config ;
-	
-	public RunMinibus( final String [] args ) {
-		if(args.length == 0){
-			log.info("Arg 1: config.xml is missing.");
-			log.info("Check http://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/atlantis/minibus/ for an example.");
-			System.exit(1);
-		}
-		config = ConfigUtils.loadConfig( args[0], new PConfigGroup() ) ;
-	}
-	
-	public final void run() {
-		Scenario scenario = ScenarioUtils.loadScenario(config);
-		
-		Controler controler = new Controler(scenario);
-		
-		controler.addOverridingModule(new PModule()) ;
-		
-		controler.run();
-	}
-	
-	public final Config getConfig() {
-		return this.config ;
-	}
+
+//	private final Config config ;
+
+//	public RunMinibus( final String [] args ) {
+////		if(args.length == 0){
+////			log.info("Arg 1: config.xml is missing.");
+////			log.info("Check http://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/atlantis/minibus/ for an example.");
+////			System.exit(1);
+////		}
+//		config = ConfigUtils.loadConfig( args[0], new PConfigGroup() ) ;
+//	}
+//
+//	public final void run() {
+//		Scenario scenario = ScenarioUtils.loadScenario(config);
+//
+//		Controler controler = new Controler(scenario);
+//
+//		controler.addOverridingModule(new PModule()) ;
+//
+//		controler.run();
+//	}
+//
+//	public final Config getConfig() {
+//		return this.config ;
+//	}
+//
+//	public static void main(final String[] args) {
+//		new RunMinibus( args ).run() ;
+//	}
+
+
+
 
 	public static void main(final String[] args) {
-		new RunMinibus( args ).run() ;
+		Config config = ConfigUtils.loadConfig( "/Users/MeyerMa/IdeaProjects/data-science-matsim/jobs-infra/docker-build/input/minibus/config_minibus_human_driven_600it.xml", new PConfigGroup() ) ;
+//		Config config = ConfigUtils.loadConfig("/Users/MeyerMa/IdeaProjects/minibus_meyer/Input/config.xml", new PConfigGroup() ) ;
+		config.network().setInputFile("/Users/MeyerMa/IdeaProjects/data-science-matsim/jobs-infra/docker-build/input/minibus/berlin-v5.5.3-1pct.output_network.xml.gz");
+		config.global().setCoordinateSystem("EPSG:31468");
+		config.global().setRandomSeed(1);
+		config.plans().setInputFile("/Users/MeyerMa/IdeaProjects/data-science-matsim/jobs-infra/docker-build/input/minibus/berlin-v5.4-1pct.plans_activity_inside_prep.xml");
+		config.plans().setRemovingUnneccessaryPlanAttributes(true);
+		config.transit().setTransitScheduleFile("/Users/MeyerMa/IdeaProjects/data-science-matsim/jobs-infra/docker-build/input/minibus/berlin-v5.5.3-1pct.output_transitSchedule_no_bus_in_spandau.xml.gz");
+		config.transit().setVehiclesFile("/Users/MeyerMa/IdeaProjects/data-science-matsim/jobs-infra/docker-build/input/minibus/berlin-v5.5.3-1pct.output_transitVehicles.xml.gz");
+//		config.households().setInputFile();
+//		config.facilities().setInputFile();
+
+		config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+		config.controler().setOutputDirectory("/Users/MeyerMa/Desktop/MA/scenarios/berlin/output/minibus_human_600it_seed1");
+
+		config.plans().setHandlingOfPlansWithoutRoutingMode(useMainModeIdentifier);
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+		config.controler().setRunId("seed_1");
+		config.controler().setLastIteration(600);
+
+
+
+
+		MainModeIdentifier mainModeIdentifier = new MainModeIdentifierImpl();
+		TripsToLegsAlgorithm algorithm = new TripsToLegsAlgorithm(mainModeIdentifier);
+
+
+
+		for (Person person: scenario.getPopulation().getPersons().values())	{
+			Plan plan = person.getSelectedPlan();
+			algorithm.run(plan);
+			for (PlanElement element : plan.getPlanElements()) {
+				if (element instanceof Activity) {
+					Activity activity = (Activity) element;
+					if (!Collections.singleton(PtConstants.TRANSIT_ACTIVITY_TYPE).contains(activity.getType())) {
+						activity.setType("h");
+					}
+				}
+			}
+		}
+
+
+
+		Controler controler = new Controler(scenario);
+
+		controler.addOverridingModule(new PModule());
+
+
+
+		// if desired, add subsidy approach here
+//		PConfigGroup pConfig = ConfigUtils.addOrGetModule(config, PConfigGroup.class);
+//		pConfig.setSubsidyApproach("perPassenger");
+
+		controler.run();
 	}
+
 }
